9958f16eeb377fb590fef945d12856888f3f5f2d
Willemijn Bresjer <wbbresjer@gmail.com>
2019-10-14 15:43:02 +0000

    Add file for my solution

--- let_const.js
+++ let_const.js
@@ -9,47 +9,12 @@
 */
 
 
-const teachers = [
+let teachers = [
 	{
 		name: 'Ashley',
 		topicArea: 'Javascript'
 	}
 ];
-
-
-const courses = ['Introducing JavaScript',
-				'JavaScript Basics',
-				'JavaScript Loops, Arrays and Objects',
-				'Getting Started with ES2015',
-				'JavaScript and the DOM',
-				'DOM Scripting By Example'];
-			
-
-var i = courses.length;
-
-
-
-function addNewTeachers(newTeachers) {
-	// TODO: write a function that adds new teachers to the teachers array 
-
-}
-
-
-function printTreehouseSummary() {
-	// TODO: fix this function so that it prints the correct number of courses and   teachers 
-	
-  for (var i = 0; i < teachers.length; i++) {
-	 console.log(`${teachers[i].name} teaches ${teachers[i].topicArea}`);
-	}
-	
-  console.log(`Treehouse has ${i} JavaScript courses, and ${teachers.length} Javascript teachers`);
-}
-
-
-addNewTeachers(newTeachers);
-printTreehouseSummary();
-
-
 
 let newTeachers = [
 	{
@@ -61,6 +26,41 @@ let newTeachers = [
 		topicArea: 'Javascript'
 	}
 ];
+
+const courses = ['Introducing JavaScript',
+				'JavaScript Basics',
+				'JavaScript Loops, Arrays and Objects',
+				'Getting Started with ES2015',
+				'JavaScript and the DOM',
+				'DOM Scripting By Example'];
+
+//var i = courses.length;
+
+
+
+function addNewTeachers(newTeachers) {
+	// TODO: write a function that adds new teachers to the teachers array 
+  teachers = teachers.concat(newTeachers);
+}
+
+
+function printTreehouseSummary() {
+	// TODO: fix this function so that it prints the correct number of courses and teachers 
+	
+  for (let i = 0; i < teachers.length; i++) {
+	 console.log(`${teachers[i].name} teaches ${teachers[i].topicArea}`);
+	}
+	
+  console.log(`Treehouse has ${courses.length} JavaScript courses, and ${teachers.length} Javascript teachers`);
+}
+
+
+addNewTeachers(newTeachers);
+printTreehouseSummary();
+
+
+
+
 
 
 
